4f1b87046ef4095630ee68b395df8b8fa7cc3636
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-06 17:55:02 +0545

    Add files via upload

---
+++ Maps.js
@@ -0,0 +1,126 @@
+const restaurant = {
+  name: "Cafe Mocha",
+  location: "Pokhara, Kathmandu, Biratnagar, Nepalgunj",
+  categories: ["Italian", "Chinese Cuisine", "Indian", "Organic"],
+  starterMenu: ["Chicken Lollipop", "Salad", "Bread", "Manchurian"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    Sun: {
+      open: 12,
+      close: 22,
+    },
+    Mon: {
+      open: 11,
+      close: 23,
+    },
+
+    Tue: {
+      open: 10,
+      close: 20,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20.00",
+    address = "Jaleshwar TownPlanning",
+  }) {
+    console.log(`Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}
+        will be delivered to ${address} at ${time}`);
+  },
+
+  //So lets add here another method now. And lets say we want a method to order just pasta. And the
+  //pasta always needs to have exactly three ingredients.
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+    //So now lets call this function.
+    //Now what i want to do here is to actually get these ingredients from a prompt window.
+  },
+};
+
+//Expamding an array using brute force approach.
+
+const arr = [7, 8, 9];
+const NewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(NewArr);
+
+//Expanding an array using spread operator.
+const OptimizedArr = [1, 2, ...arr];
+//Here it prints the whole array.
+console.log(OptimizedArr);
+//but by using spread operator it actually prints all the individual elements of the new array.
+console.log(...OptimizedArr);
+//Whenever we need the elements of an array individually then only we use spread operator. Using the spread
+//operator you can actually create a whole new array.
+
+//There are two important use-cases of spread operator.
+//1: To create the shallow copies of arrays.
+//Here we actually copy the whole array of mainMenu into the newly created array called as mainmenuCopy.
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//To merge the two arrays together.
+const Menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(Menu);
+// Spread operator doesnot only work for arrays but it also works for all iterables.
+//Here iterables simply means all arrays, strings, maps, sets etc but not objects.
+//Lets now understand spread operator along with strings. Our task here is to print the individual
+//letters of this string.
+
+const str = "Shishiranjan";
+//With this line of code below we actually print all the separate elements of the array.
+const letters = [...str, " ", "S."];
+console.log(letters);
+
+//Lets now actually write our own function that accepts multiple arguments and then use the spread operator to
+//actually pass those arguments.
+//Each element will be defined here by a prompt window. Prompt window has nothing to do with the spread
+//operator.It is just a way so that we can actually input the data ourselves.
+//Real world example.
+const ingredients = [
+  prompt("Let's make pasta! Ingredient 1?)"),
+  prompt("ingredient 2?"),
+  prompt("Ingredient 3"),
+];
+
+console.log(ingredients);
+
+//Now how should we actually call the order pasta function.
+restaurant.orderPasta(...ingredients);
+
+//In ES version of 2018 the spread operator also works on objects even though objects are not iterables.
+
+//Easiest way to create a map.
+//In mapkeyword there is always two arguments. First one is the key value and second one is the actual value.
+
+const rest = new Map();
+
+//To add some element into the map, we use set method.
+
+rest.set(("name", "Indian Cuisine"));
+rest.set((1, "Jaleshwar", "Kathmandu"));
+console.log(rest.set(("2", "India", "UK")));
+
+rest.set("categories", ["Italian", "Chinese Cuisine", "Indian", "Organic"])
+  .set("open", 11).set("close", 23).set(true, "We are open").set
+  (false, "We are closed");
+
+
+//To retrieve the element from the set we use get method.
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+//Here if the time is between 11 and 23 then we print the message acoording to open and close key value 
+//pair
+
+const time = 0;
+console.log(rest.get(time > rest.get('open') && rest.get(rest.get('close')));
+
+
